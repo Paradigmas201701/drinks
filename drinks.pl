1b5e74e405e19ceff722da772f1492fb4e5dd21f
ricardocanela <ricardo_canela123@hotmail.com>
2017-05-09 12:00:05 -0300

    adicionados ingredientes e inclusao

---
+++ drinks.pl
@@ -0,0 +1,13 @@
+
+%ingrediente(rum).
+ingrediente(acucar).
+ingrediente(sucoDeLimao).
+ingrediente(aguaComGas).
+ingrediente(hortela).
+ingrediente(tequila).
+ingrediente(sal).
+ingrediente(licorDeLaranja).
+ingrediente(refrigeranteDeCola).
+
+incluir(X):- assert(ingrediente(X)).
+
